2759b8ed752754b1d7028080479ef95e9f191d33
fiction537 <les@studiofiction.com>
2011-03-27 20:47:23 -0700

    initial commit of static app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatApp::Application.config.secret_token = '62cf492d8a897930962ab31f22c2e9a0ef10c583546a28c3a610db536222b21388d5c696809586413efd4bc827f7bec5711761f4b2b47436a8ec11b4823970e8'
